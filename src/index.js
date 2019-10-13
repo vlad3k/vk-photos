70c9650f873eb16d999a54049c4df6ef18a3dacd
vlad3k <vlad3k@bk.ru>
2019-10-13 13:18:07 +0500

    dispatching events

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import { store } from './store/configureStore'
 
-import App from './components/app/App'
+import App from './containers/app'
 
 import './index.css'
 
